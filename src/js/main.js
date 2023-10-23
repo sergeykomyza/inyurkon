42436dc27b1302c97940ee82f05c4cb5bae742ce
sergey komyza <sergeykomyza@gmail.com>
2023-10-23 13:50:57 +0300

    параллакс, логотипы, фоновое изображение на услугах, новости 3 шт, новые пункты меню

--- src/js/main.js
+++ src/js/main.js
@@ -54,113 +54,132 @@ const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
 }
 
 // ==================================================\Fancybox
-Fancybox.bind("[data-fancybox]", {
-});
-
-Fancybox.bind('[data-fancybox="news"]', {
-    Thumbs: {
-      type: 'none',
-      autoStart: false,
-    },
-  });
+const fancybox = ()=> {
+    Fancybox.bind("[data-fancybox]", {
+    });
+    
+    Fancybox.bind('[data-fancybox="news"]', {
+        Thumbs: {
+          type: 'none',
+          autoStart: false,
+        },
+    });
+}
 
 // ================================================== МАСКА ДЛЯ ИНПУТОВ (https://github.com/RobinHerbots/Inputmask)
-$(document).ready(function () {
+const inputMask = () => {
     $(".js-maskPhone").inputmask({
         mask: "+7 999 999 99 99",
         clearIncomplete: true
     });
-    $('.email').inputmask({
-        mask: "*{1,20}[.*{1,20}]@*{1,20}.*{2,4}",
-        clearIncomplete: true
-    //     greedy: false,
-    //     onBeforePaste: function (pastedValue, opts) {
-    //         pastedValue = pastedValue.toLowerCase();
-    //         return pastedValue.replace("mailto:", "");
-    //     },
-    //     definitions: {
-    //         '*': {
-    //             validator: "[0-9A-Za-z-а-я-]",
-    //             casing: "lower"
-    //         }
-    //     }
-    });
-    $(".js-maskDate").inputmask({
-        mask: "99/99/9999",
-        clearIncomplete: true,
-        'placeholder': 'dd/mm/yyyy'
-    });
-});
+}
 
 // ================================================== СЛАЙДЕР SWIPER (https://swiperjs.com/get-started) 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
+const sliders = () => {
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'vertical',
+        loop: true,
+    
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+}
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
+// ================================================== parallax
+const parallax = (elem, startPos, marker)=> {
+    gsap.to(elem, {
+        transform: `translateY(${innerHeight / 1}px)`,
+        ease: "none",
+        scrollTrigger: {
+            trigger: elem,
+            start: startPos,
+            end: "+=1500",
+            scrub: true,
+            // snap: {
+            //     snapTo: 0.5, duration: 1, delay: 0.3, ease: 'back.out(1)'
+            // },
+            // markers: {
+            //     fontSize: '20px',
+            //     startColor: 'green',
+            //     endColor: 'red'
+            // },
+            markers: marker
+        }
+    });
+}
+    
+parallax('.main-page .js-parallax1', "60% 50%", false)
+parallax('.main-page .js-parallax2', "40% 50%", false)
+parallax('.main-page .js-parallax3', "-20% 50%", false)
+// ================================================== КАРТА, ОТЛОЖЕННАЯ ЗАГРУЗКА (ЧТОБЫ УЛУЧШИТЬ ПОКАЗАТЕЛИ - PageSpeed Insights)
+const map = ()=> {
+    document.addEventListener('DOMContentLoaded', function () {
+        setTimeout(function() {
+            var headID = document.getElementsByTagName("body")[0];         
+            var newScript = document.createElement('script');
+            newScript.type = 'text/javascript';
+            newScript.src = 'https://api-maps.yandex.ru/2.1/?lang=ru_RU';
+            headID.appendChild(newScript);
+        }, 500);
+        setTimeout(function() {
+                var myMap = new ymaps.Map("map", {
+                center: [55.761147, 37.651259],
+                zoom: 16,
+                controls: ['smallMapDefaultSet']
+            }, {
+                searchControlProvider: 'yandex#search'
+            });
+    
+            myGeoObject = new ymaps.GeoObject({
+                geometry: {
+                    type: "Point"
+                },
+            });
+            myMap.geoObjects
+                .add(myGeoObject)
+                .add(new ymaps.Placemark([55.761112, 37.652292], {
+                    balloonContent: '<strong>101000, г. Москва, улица Покровка 40Б, подъезд 3</strong>',
+                    iconCaption: 'Покровка 40Б'
+                }, {
+                    preset: 'islands#blueCircleDotIconWithCaption',
+                    iconCaptionMaxWidth: '200'
+                }));
+    
+            myMap.setType('yandex#publicMap');
+    
+            myMap.behaviors.disable('scrollZoom');
+            //на мобильных устройствах... (проверяем по userAgent браузера)
+            if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+                //... отключаем перетаскивание карты
+                myMap.behaviors.disable('drag');
+            }
+        }, 1000);
+    });
+}
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
-});
-// ================================================== 
+// ==================================================
 headerLogic()
 mMenuToggle()
+inputMask()
 if (document.querySelector('.services')) {
     tabs('.services-choose__buttons', '.services-choose__button', '.services-choose__content', 'active');
 }
-// ================================================== КАРТА, ОТЛОЖЕННАЯ ЗАГРУЗКА (ЧТОБЫ УЛУЧШИТЬ ПОКАЗАТЕЛИ - PageSpeed Insights)
-document.addEventListener('DOMContentLoaded', function () {
-    setTimeout(function() {
-        var headID = document.getElementsByTagName("body")[0];         
-        var newScript = document.createElement('script');
-        newScript.type = 'text/javascript';
-        newScript.src = 'https://api-maps.yandex.ru/2.1/?lang=ru_RU';
-        headID.appendChild(newScript);
-    }, 500);
-    setTimeout(function() {
-            var myMap = new ymaps.Map("map", {
-            center: [55.761147, 37.651259],
-            zoom: 16,
-            controls: ['smallMapDefaultSet']
-        }, {
-            searchControlProvider: 'yandex#search'
-        });
-
-        myGeoObject = new ymaps.GeoObject({
-            geometry: {
-                type: "Point"
-            },
-        });
-        myMap.geoObjects
-            .add(myGeoObject)
-            .add(new ymaps.Placemark([55.761112, 37.652292], {
-                balloonContent: '<strong>101000, г. Москва, улица Покровка 40Б, подъезд 3</strong>',
-                iconCaption: 'Покровка 40Б'
-            }, {
-                preset: 'islands#blueCircleDotIconWithCaption',
-                iconCaptionMaxWidth: '200'
-            }));
-
-        myMap.setType('yandex#publicMap');
-
-        myMap.behaviors.disable('scrollZoom');
-        //на мобильных устройствах... (проверяем по userAgent браузера)
-        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-            //... отключаем перетаскивание карты
-            myMap.behaviors.disable('drag');
-        }
-    }, 1000);
-});
+fancybox()
+if(document.querySelector('#map')){
+    map()
+}
 
